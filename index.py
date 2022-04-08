563c37723256063349ccc4a65019e601fe3a2228
Aurelie Gedeon <agedeon540@gmail.com>
2022-04-08 18:19:53 -0400

    created columns for todo list

--- index.py
+++ index.py
@@ -1,10 +1,19 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 # tells app where database is located
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)  # initialize database
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # this is so the user can't put in a blank
+    content = db.Column(db.string(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    data_created = db.Column(db.DateTime, default=datetime.utcnow)
 
 
 @app.route('/')
