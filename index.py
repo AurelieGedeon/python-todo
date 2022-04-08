cc26e0b460b470d3a2af733accd1cadfa39c785d
Aurelie Gedeon <agedeon540@gmail.com>
2022-04-08 18:12:06 -0400

    initialized database; started todo part of application

--- index.py
+++ index.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# tells app where database is located
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)  # initialize database
 
 
 @app.route('/')
