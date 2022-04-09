a6f588ccc57b850fd8bcc764e58f2b47c9fcf1f4
Aurelie Gedeon <agedeon540@gmail.com>
2022-04-08 20:42:03 -0400

    added css to display tables; added delete method

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime, date
 
@@ -24,7 +24,44 @@ class Todo(db.Model):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+
+        except:
+            "Unable to add task"
+    else:
+        tasks = Todo.query.order_by(Todo.data_created).all()
+        return render_template('index.html', tasks=tasks)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "Unable to delete task"
+
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task_to_update = Todo.query.get_or_404(id)
+
+    try:
+        db.session.update(task_to_update)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return "Unable to update task"
 
 
 if __name__ == '__main__':
